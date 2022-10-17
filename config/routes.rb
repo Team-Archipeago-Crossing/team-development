d7dda7c03ad841997be0d3958dcfc3a89a3410b6
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-10-17 07:25:36 +0000

    Add About-page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   
+  get "about" => "homes#about", as: "about"
   resources :items, only: [:index, :show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
