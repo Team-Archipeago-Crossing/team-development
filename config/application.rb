493449d58373224007e70aea77768778152573a3
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-10-21 04:47:49 +0000

    [Add]管理者トップページのviewを編集しました

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module WorkGithub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,5 +19,8 @@ module WorkGithub
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
+    
+    #timestamp日本時間にするため追加
+    config.time_zone = 'Tokyo'
   end
 end
