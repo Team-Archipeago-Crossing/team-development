c36db1de0b2822fb63c7171601f29bee8211d10b
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-10-26 10:50:51 +0000

    [Fix]管理者側　会員情報編集エラーメッセージが出るように修正しました。

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,8 +13,8 @@ class Admin::CustomersController < ApplicationController
 	end
 	
 	def update
-		customer = Customer.find(params[:id])
-		if customer.update(customer_params)
+		@customer = Customer.find(params[:id])
+		if @customer.update(customer_params)
 			redirect_to admin_customer_path(params[:id]), notice: "会員情報を編集しました"
 		else
 			render :edit
