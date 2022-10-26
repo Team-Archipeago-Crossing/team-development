132a063f2e8d8dd6aefdaa5d1f8bf0b087cc153d
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-10-26 14:12:47 +0000

    売切商品の一覧画面でのカーソル挙動変更、カートに追加できない機能のバグを回避

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,17 @@
 class Public::CartItemsController < ApplicationController
 
   def create
-    customer = current_customer
-    cart_items = customer.cart_items
-    if cart_items.pluck(:item_id).include?(params[:cart_item][:item_id].to_i)
-      item = cart_items.find_by(item_id: params[:cart_item][:item_id].to_i)
-      item.amount += params[:cart_item][:amount].to_i
-      item.save
+    cart_items = current_customer.cart_items
+    item_id = params[:cart_item][:item_id]
+    return redirect_back fallback_location: item_path(item_id) unless Item.find(item_id).is_active
+    if cart_items.pluck(:item_id).include?(item_id.to_i)
+      cart_item = cart_items.find_by(item_id: item_id)
+      cart_item.amount += params[:cart_item][:amount].to_i
     else
       cart_item = CartItem.new(cart_item_params)
       cart_item.customer_id = customer.id
-      cart_item.save
     end
+    cart_item.save
   	redirect_to cart_items_path
   end
 
@@ -49,14 +49,13 @@ class Public::CartItemsController < ApplicationController
     params.require(:cart_item).permit(:amount, :item_id)
   end
 
-
-def calc_subtotal
-  @customer = current_customer
-  @cart_items = @customer.cart_items
-  @subtotal = 0
-  @cart_items.each do |item|
-    @subtotal += (item.item.price * $tax_rate).floor * item.amount
+  def calc_subtotal
+    @customer = current_customer
+    @cart_items = @customer.cart_items
+    @subtotal = 0
+    @cart_items.each do |item|
+      @subtotal += (item.item.price * $tax_rate).floor * item.amount
+    end
   end
-end
 
 end
