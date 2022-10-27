c33c7398fb43cdc6e22292b83457ea04a4f847fb
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-10-27 04:47:59 +0000

    カート追加時・注文確認時の不具合修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,11 +5,13 @@ class Public::ItemsController < ApplicationController
 	end
 
 	def show
-    @item = Item.find(params[:id])
-    @cart_item = CartItem.new
-    purchase_history = current_customer.past_order_details.reverse_order
-    @order_times = purchase_history.count{|i| i.item_id == @item.id}
-    @last_order = purchase_history.find_by(item_id: @item.id).order if @order_times > 0
+        @item = Item.find(params[:id])
+        @cart_item = CartItem.new
+        if customer_signed_in?
+            purchase_history = current_customer.past_order_details.reverse_order
+            @order_times = purchase_history.count{|i| i.item_id == @item.id}
+            @last_order = purchase_history.find_by(item_id: @item.id).order if @order_times > 0
+        end
 	end
 
 end
